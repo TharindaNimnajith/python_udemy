52f8f6d9f10f68ce61460a0ea8e0fc736ec85636
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-01-19 22:18:13 +0530

    test

---
+++ src/intro.py
@@ -0,0 +1,20 @@
+print('Hello World!')
+
+name = input('What is your name? ')
+print('Hello ' + name)
+
+# fundamental data types
+# int
+# float
+# bool
+# str
+# tuple
+# list
+# set
+# dict
+#
+# classes (custom types)
+#
+# specialized data types (packages and modules from libraries)
+#
+# None
