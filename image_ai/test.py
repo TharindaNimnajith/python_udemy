683ec2de53efaf8810a3b85dc339487086d5f538
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-01-22 20:37:24 +0530

    test

---
+++ image_ai/test.py
@@ -0,0 +1,15 @@
+import os
+
+from imageai.Classification import ImageClassification
+
+execution_path = os.getcwd()
+
+prediction = ImageClassification()
+prediction.setModelTypeAsResNet50()
+prediction.setModelPath(os.path.join(execution_path, 'resnet50_imagenet_tf.2.0.h5'))
+prediction.loadModel()
+
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, '1.jpg'), result_count=5)
+
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction, ' : ', eachProbability)
