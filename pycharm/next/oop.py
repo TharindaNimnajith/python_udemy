58283b1f61d66e7d57119d3741207bacfd4f0b17
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-01-29 13:48:38 +0530

    test

--- pycharm/next/oop.py
+++ pycharm/next/oop.py
@@ -1,27 +1,3 @@
-class Cat:
-    species = 'mammal'
-
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-
-# 1. Instantiate the Cat object with 3 cats.
-peanut = Cat('Peanut', 3)
-garfield = Cat('Garfield', 5)
-snickers = Cat('Snickers', 1)
-
-
-# 2. Create a function that finds the oldest cat.
-def get_oldest_cat(*args):
-    return max(args)
-
-
-# 3. Print out: 'The oldest cat is x years old.' - x will be the
-# oldest cat age by using the function in #2.
-print(f'The oldest cat is {get_oldest_cat(peanut.age, garfield.age, snickers.age)} years old.')
-
-
 class SuperList(list):
     def __len__(self):
         return 1000
