842078d349825d8a0e31d64c6c1f26d84dbcb59f
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-02-03 11:25:14 +0530

    test

--- pycharm/modules/guessing_game.py
+++ pycharm/modules/guessing_game.py
@@ -1,22 +1,17 @@
 import sys
-
-print(sys.path)
-
-
 from random import randint
-import sys
 
 answer = randint(int(sys.argv[1]), int(sys.argv[2]))
 
 while True:
     try:
-        guess = int(input(f'guess a number {sys.argv[1]}~{sys.argv[2]}:  '))
+        guess = int(input(f'Guess a number {sys.argv[1]}~{sys.argv[2]}: '))
         if 0 < guess < 11:
             if guess == answer:
-                print('you are a genius!')
+                print('Correct!')
                 break
         else:
-            print('hey bozo, I said 1~10')
+            print('Need a number in 1~10 range.')
     except ValueError:
-        print('please enter a number')
+        print('Please enter a number.')
         continue
---
+++ pycharm/modules/modules.py
@@ -0,0 +1,29 @@
+# modules
+
+# three main types of modules in python
+# 1. those you write yourself
+# 2. those you install from external sources
+# 3. those that are preinstalled with python - standard library
+
+# standard library's useful modules:
+# string
+# re
+# datetime
+# math
+# random
+# os
+# multiprocessing
+# subprocess
+# socket
+# email
+# json
+# doctest
+# unittest
+# pdb
+# argparse
+# sys
+
+
+import sys
+
+print(sys.path)
